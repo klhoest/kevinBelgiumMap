fbdb6a557a9995bcce8fbd091e36d76104a9417b
Kevin desktop <k.lhoest@groupeonepoint.com>
2019-10-02 23:31:25 +0200

    find brussel region and insert it in a naive way to the province array

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -2,6 +2,7 @@ import com.google.gson.Gson
 
 const val PROVINCE_FILE_NAME = "belgium/admin_level_6.geojson"
 const val REGION_FILE_NAME = "belgium/admin_level_4.geojson"
+const val BRUSSELS_REGION_ID = 54094
 
 val gson = Gson()
 
@@ -12,12 +13,18 @@ fun main(args: Array<String>) {
         println("could not open $PROVINCE_FILE_NAME")
         return
     }
-    print(fullGeoModel.type)
 
     val regions = jsonDeserializer.extractPojo(REGION_FILE_NAME, FullGeoModel::class.java)
     if (regions == null) {
         println("could not open $REGION_FILE_NAME")
         return
     }
-    print(regions.features?.first())
+    val brusselsRegion = regions.features.find { it -> it.id == BRUSSELS_REGION_ID }
+    if (brusselsRegion == null) {
+        println("could not find any region with id = $BRUSSELS_REGION_ID")
+        return
+    }
+
+    fullGeoModel.features += brusselsRegion
+    val outputJson: String = gson.toJson(fullGeoModel)
 }
